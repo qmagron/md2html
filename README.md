1c1989a2329df0bcca37418e6009b90d9b19960e
Quentin Magron <Quentin.Magron@ulb.be>
2022-11-06 14:39:03 +0100

    Command line arguments parsing

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # md2html
 
 A simple and flexible markdown to html transpiler.
+
+## Install
+
+    npm i md2html
+
+## Usage
+
+### CLI
+
+    node md2html input.md [output.html]
+    node md2html input.md -
+    cat input.md | node md2html - output.md
+
+Use `-` to read from stdin or to write to stdout.
+
+### JavaScript
+
+```js
+const md2html = require('md2html');
+const html = md2html(md);
+```
+
+## Syntax
+
+The basic Markdown syntax is supported as well as some extra features.
+Moreover, Markdown is easily expandable by directly using HTML tags.
+
+- [Basic syntax](https://www.markdownguide.org/basic-syntax/)
+- [Extended syntax](https://www.markdownguide.org/extended-syntax/)
+- [Hacks](https://www.markdownguide.org/hacks/)
---
+++ md2html.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+
+/**
+ * Perform a Markdown -> HTML transpilation.
+ *
+ * @param   {String}  markdown  Markdown text to transpile
+ * @return  {String}  Generated HTML
+ */
+ function transpile(markdown) {
+  let html = markdown.toString();
+
+  // TODO
+
+  return html;
+}
+
+
+module.exports = transpile;
+
+
+/**
+ * Command line script.
+ *
+ * The input and output can be stdin and stdout by replacing
+ * the relevant file name with `-`.
+ *
+ * @param  {String}  in   Input file
+ * @param  {String}  out  Output file [optional]
+ */
+if (require.main == module) {
+  const argv = process.argv.slice(2);
+
+  // Change '-' for stdin
+  const srcFile = argv[0] == '-' ? '/dev/stdin' : argv[0];
+  const srcPath = path.parse(srcFile);
+
+  // Change '-' for stdout
+  let dstFile = argv[1] == '-' ? '/dev/stdout' : argv[1];
+
+  // Set default output
+  if (argv[0] != '-') {
+    dstFile = dstFile ?? `${srcPath.dir}/${srcPath.name}.html`;
+  }
+  const dstPath = path.parse(dstFile);
+
+  fs.access(srcFile)
+    .then(() => fs.access(dstPath.dir))
+    .then(() => fs.readFile(srcFile))
+    .then(transpile)
+    .then(html => fs.writeFile(dstFile, html))
+    .catch(console.error);
+}
